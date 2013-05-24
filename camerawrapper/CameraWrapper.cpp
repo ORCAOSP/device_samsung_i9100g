f771222a93d25b06801d6d6fc36af3a7e52baf62
Orca Project <orcaproject@yahoo.com>
2013-05-24 00:37:07 -0300

    camera: fix exposure compensation, reenable software HDR
    camera: fix exposure compensation, reenable software HDR
    Change-Id: If8f83f375655ff4a8ac07aff21e2910d4778e733

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -22,7 +22,7 @@
 */
 
 
-#define LOG_NDEBUG 0
+//#define LOG_NDEBUG 0
 //#define LOG_PARAMETERS
 
 #define LOG_TAG "CameraWrapper"
@@ -99,8 +99,14 @@ static char * camera_fixup_getparams(int id, const char * settings)
     params.unflatten(android::String8(settings));
 
     // fix params here
+    params.set(android::CameraParameters::KEY_SUPPORTED_ISO_MODES, iso_values[id]);
 
-    params.set("iso-values", iso_values[id]);
+    /* Back camera */
+    if(id == 0) {
+        params.set(android::CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "1.0");
+        params.set(android::CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-3");
+        params.set(android::CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, "3");
+    }
 
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
@@ -111,22 +117,25 @@ static char * camera_fixup_getparams(int id, const char * settings)
 
 char * camera_fixup_setparams(int id, const char * settings)
 {
+    const char* isoMode;
+
     android::CameraParameters params;
     params.unflatten(android::String8(settings));
 
-
     // fix params here
 
-    const char* isoMode = params.get("iso");
-    if(isoMode) {
-        if(!strcmp(isoMode, "ISO100"))
-            params.set("iso", "100");
-        else if(!strcmp(isoMode, "ISO200"))
-            params.set("iso", "200");
-        else if(!strcmp(isoMode, "ISO400"))
-            params.set("iso", "400");
-        else if(!strcmp(isoMode, "ISO800"))
-            params.set("iso", "800");
+    /* ISO modes */
+    if(params.get(android::CameraParameters::KEY_ISO_MODE)) {
+        isoMode = params.get(android::CameraParameters::KEY_ISO_MODE);
+
+        if(strcmp(isoMode, "ISO100") == 0)
+            params.set(android::CameraParameters::KEY_ISO_MODE, "100");
+        else if(strcmp(isoMode, "ISO200") == 0)
+            params.set(android::CameraParameters::KEY_ISO_MODE, "200");
+        else if(strcmp(isoMode, "ISO400") == 0)
+            params.set(android::CameraParameters::KEY_ISO_MODE, "400");
+        else if(strcmp(isoMode, "ISO800") == 0)
+            params.set(android::CameraParameters::KEY_ISO_MODE, "800");
     }
 
     android::String8 strParams = params.flatten();
